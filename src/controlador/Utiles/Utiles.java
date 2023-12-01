21d8cb820f3222ec1eab15b8afbacf22abaa1047
is4ac <isaac.ramon@unl.edu.ec>
2023-12-01 09:27:23 -0500

    Cambios utiles

--- src/controlador/Utiles/Utiles.java
+++ src/controlador/Utiles/Utiles.java
@@ -4,24 +4,7 @@
  */
 package controlador.Utiles;
 
-import modelo.Tramite;
-
-
 public class Utiles {
-        
-        public Integer calcularTiempo(Tramite tramite){
-                switch (tramite.getNombre()) {
-                        case "CAMBIAR":
-                                        return 4;
-                        case "ACTUALIZAR":
-                                        return 7;
-                        case "ASIGNAR":
-                                        return 15;
-                        case "RECUPERAR":
-                                        return 3;
-                        default:
-                                throw new AssertionError();
-        }
     //Codigo para validar la cedula
     /*public static boolean validadorDeCedula(String cedula) {
         boolean cedulaCorrecta = false;
@@ -52,5 +35,4 @@ public class Utiles {
                     cedulaCorrecta = false;
                 }
         } */
-        }
 }
